5736bba7669e0b82eec6dd7af9aabe2a7b5ea923
TicklishReboots <67234652+TicklishReboots@users.noreply.github.com>
2024-01-25 23:18:58 +0100

    add hacky mobile view

--- src/pages/Sequencer.tsx
+++ src/pages/Sequencer.tsx
@@ -1,17 +1,30 @@
 import TransportSection from "../components/TransportSection";
 import Track from "../components/Track";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import Lamps from "../components/Lamps";
+import { useMediaQuery } from "../hooks/useMediaQuery";
 
 const Sequencer = () => {
   const lampsRef = useRef<HTMLInputElement[]>([]);
   const initialScale = ["C5", "B4", "A4", "G4", "F4", "E4", "D4", "C4"];
-  // const [scale, setScale] = useState<string[]>(initialScale);
-  const scale = initialScale;
+  const [scale, setScale] = useState<null | string[]>(null);
+  const isTablet = useMediaQuery("(max-width: 1023px)");
+  const isMobile = useMediaQuery("(max-width: 767px)");
+
+  useEffect(() => {
+    if (isMobile) {
+      setScale(initialScale.slice(0, 4));
+    } else if (isTablet) {
+      setScale(initialScale.slice(0, 6));
+    } else {
+      setScale(initialScale);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isMobile]);
 
   return (
     <>
-      {scale.map((note, index) => (
+      {scale?.map((note, index) => (
         <Track key={index} note={note} rowIndex={index} />
       ))}
       <Lamps lampsRef={lampsRef} />
