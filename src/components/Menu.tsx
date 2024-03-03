4913af201098ac728b86f05910aa790b2069cd30
TicklishReboots <67234652+TicklishReboots@users.noreply.github.com>
2024-03-03 20:23:02 +0100

    add mui - add mui button, dropdown - add theme colors

---
+++ src/components/Menu.tsx
@@ -0,0 +1,11 @@
+import Button from "@mui/material/Button";
+
+const Menu = () => {
+  return (
+    <div className="fixed top-0">
+      <Button variant="contained">Login</Button>
+    </div>
+  );
+};
+
+export default Menu;
